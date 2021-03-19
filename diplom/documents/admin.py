6a9d3f83367a7cf29552561ebd1e3c0b0cd37705
alexey <cuteracoon@gmail.com>
2021-03-19 14:35:50 +0600

    added models for documents app

--- diplom/documents/admin.py
+++ diplom/documents/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TenderDoc
 
-# Register your models here.
+
+admin.site.register(TenderDoc)
--- diplom/documents/models.py
+++ diplom/documents/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+## Products
+
+class BrandProduct(models.Model):
+    brand = models.CharField(max_length=120, unique=True)
+
+
+class TypeProduct(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    type = models.ForeignKey(TypeProduct, on_delete=models.CASCADE)
+    brand = models.ForeignKey(BrandProduct, on_delete=models.CASCADE)
+
+
+## Documents
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    state = models.BooleanField(default=True)
+
+
+class Sale(models.Model):
+    class Meta:
+        abstract = True
+
+    customer = models.CharField(max_length=120, verbose_name="Заказчик")
+    document = models.ImageField(verbose_name="Документ")
+    price = models.PositiveIntegerField()
+    slug = models.SlugField(unique=True)
+    guarantee = models.PositiveIntegerField(default=0)
+    product = models.ManyToManyField(Product)
+
+
+class RetailSales(Sale):
+    guarantee_status = models.BooleanField(default=True)
+
+
+class TenderDoc(Sale):
+    organizer = models.CharField(max_length=240, verbose_name="Организатор закупки")
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, blank=False, null=False, verbose_name="Клиника")
